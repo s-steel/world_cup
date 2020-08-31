3229adbb235f4f3d7f2c830c412ecf21056aad4d
Sean Steel <seanrsteel@gmail.com>
2020-08-31 17:12:46 -0600

    Add eliminated? and add_player method and tests

--- lib/team.rb
+++ lib/team.rb
@@ -1,8 +1,17 @@
 class Team
-  attr_reader :name
-  def initialize(name)
-    @name = name
-  end 
+  attr_reader :country, :players
+  attr_accessor :eliminated
+  def initialize(country)
+    @country = country
+    @eliminated = false
+    @players = []
+  end
 
+  def eliminated?
+    @eliminated
+  end
 
+  def add_player(player) 
+    @players << player
+  end
 end
--- test/team_test.rb
+++ test/team_test.rb
@@ -14,7 +14,31 @@ class TeamTest < MiniTest::Test
   def test_has_attributes
     team = Team.new("France")
 
-    assert_equal "France", team.name
+    assert_equal "France", team.country
   end
+
+  def test_can_be_eliminated
+    team = Team.new("France")
+
+    assert_equal false, team.eliminated?
+
+    team.eliminated = true
+
+    assert_equal true, team.eliminated?
+  end
+
+  def test_add_players
+    team = Team.new("France")
+    mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+    pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+
+    team.add_player(mbappe)
+
+    assert_equal [mbappe], team.players
+
+    team.add_player(pogba)
+
+    assert_equal [mbappe, pogba], team.players
+  end 
 
 end
